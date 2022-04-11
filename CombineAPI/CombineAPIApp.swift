ce1577a969a0f5c587b0ce031b9cf7fd81529b7b
Shane Claussen <sclaussen@yahoo.com>
2022-04-11 10:02:54 -0700

    Initial Commit

---
+++ CombineAPI/CombineAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAPIApp.swift
+//  CombineAPI
+//
+//  Created by Shane Claussen on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CombineAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
